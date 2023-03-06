6c768e219407b0d2b9feba686f40801c22da49af
chazzox <caylott@outlook.com>
2023-03-06 17:34:00 +0000

    nuking astro image

--- devlogs/astro.config.mjs
+++ devlogs/astro.config.mjs
@@ -2,7 +2,6 @@ import { defineConfig } from "astro/config";
 import vercel from "@astrojs/vercel/static";
 import svelte from "@astrojs/svelte";
 import tailwind from "@astrojs/tailwind";
-import image from "@astrojs/image";
 import remarkMermaid from "astro-diagram/remark-mermaid";
 import prefetch from "@astrojs/prefetch";
 import mdx from "@astrojs/mdx";
@@ -10,16 +9,9 @@ import mdx from "@astrojs/mdx";
 export default defineConfig({
     output: "static",
     adapter: vercel(),
-    integrations: [image(), svelte(), prefetch(), tailwind(), mdx()],
+    integrations: [svelte(), prefetch(), tailwind(), mdx()],
     markdown: {
         remarkPlugins: [remarkMermaid, "remark-math"],
-        rehypePlugins: [
-            [
-                "rehype-katex",
-                {
-                    // Katex plugin options
-                }
-            ]
-        ]
+        rehypePlugins: ["rehype-katex"]
     }
 });
