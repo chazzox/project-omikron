434d4768aaf18cd8817faca77508ebb6a7ba5e9f
charlie.aylott <charlie.aylott@matillion.com>
2024-01-28 18:14:04 +0000

    feat: more ui scaffolding

--- src/main.gd
+++
@@ -1,7 +0,0 @@
-extends Node2D
-
-func _on_button_pressed():
-	get_tree().root.content_scale_size = get_tree().root.content_scale_size*10
-
-func _on_ready():
-	DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN)
---
+++ src/main_menu_scene/main_menu.gd
@@ -0,0 +1,10 @@
+extends Node2D
+
+func _on_ready():
+	DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN)
+
+func _on_start_game_pressed():
+	get_tree().change_scene_to_file("res://src/game_level/game_level.tscn")
+
+func _on_credits_button_pressed():
+	get_tree().change_scene_to_file("res://src/credits_scene/credits_menu.tscn")
