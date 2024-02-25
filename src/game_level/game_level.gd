90e9d9c302b9f34538e302b58f1347079da1013c
charlie.aylott <caylott@outlook.com>
2024-02-25 17:01:40 +0000

    feat: added blur on pause menu background, and buttons do things

--- src/game_level/game_level.gd
+++ src/game_level/game_level.gd
@@ -1,5 +1,30 @@
 extends Node
 
+@onready var pause_menu = $CanvasLayer/PauseMenu
+
+var is_paused = false
+
 func _ready():
-	print("game scene loaded")
+	var playback = GlobalAudio.get_playback_position()
 	GlobalAudio.stop()
+	GlobalAudio.set_meta("pos", playback)
+	
+
+func _process(delta):
+	if Input.is_action_just_pressed("pause"):
+		print("handling pause: " + str(is_paused))
+		handlePause()
+
+
+func handlePause():
+	if is_paused:
+		var playback = GlobalAudio.get_playback_position()
+		GlobalAudio.stop()
+		GlobalAudio.set_meta("pos", playback)
+		pause_menu.hide()
+		Engine.time_scale = 1
+	else:
+		GlobalAudio.play(GlobalAudio.get_meta("pos", 0))
+		pause_menu.show()
+		Engine.time_scale = 0
+	is_paused = !is_paused
---
+++ src/pause_menu/pause_menu.gd
@@ -0,0 +1,17 @@
+extends Node
+
+@onready var main = $"../../"
+
+func _on_resume_pressed():
+	main.handlePause()
+	pass # Replace with function body.
+
+func _on_settings_pressed():
+	pass # Replace with function body.
+
+func _on_main_menu_pressed():
+	get_tree().change_scene_to_file("res://src/main_menu_scene/main_menu.tscn")
+
+func _on_quit_pressed():
+	get_tree().quit()
+
